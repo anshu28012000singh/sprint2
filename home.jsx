22a3a0c5dc9fad7eb373b2612453cfd42ba707b8
anshu28012000singh <93023678+anshu28012000singh@users.noreply.github.com>
2021-12-01 14:11:22 +0530

    Add files via upload

---
+++ home.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Home extends React.Component {
+    render() { 
+        return <div className=".text-success">
+            WELCOME TO THE COMPLAINT PORTAL 
+        </div>;
+    }
+}
+ 
+export default Home;
---
+++ success.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class Success extends React.Component {
+    render() {
+        return (
+            <div>
+                {alert("saved")}
+            </div>
+        )
+    }
+}
+export default Success;
